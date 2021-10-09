556257ea9236e933a6e62b6b1bb6e8d3618fc8d1
topla <gilotmaxime1@gmail.com>
2021-10-09 17:38:58 -0400

    Change line 5

--- src/gilot/Main.java
+++ src/gilot/Main.java
@@ -2,7 +2,7 @@ package gilot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, My name is Maxime");
+        System.out.println("Hello, My name is Maxime, I love programming");
         System.out.println("How's everything?");
     }
 }
